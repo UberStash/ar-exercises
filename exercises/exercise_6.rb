f1cbc4cb141d69b94df755605682735f3ae827b6
UberStash <expiredserver@gmail.com>
2020-09-15 17:44:51 +0000

    added in class relations and created employees data in DB

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "James", last_name: "Smith", hourly_rate: 14)
+@store1.employees.create(first_name: "Bob", last_name: "Loblaw", hourly_rate: 200)
+
+@store2 = Store.find_by(id: 2)
+@store2.employees.create(first_name: "Mother", last_name: "Jones", hourly_rate: 20)
+@store2.employees.create(first_name: "Happy", last_name: "Gilmour", hourly_rate: 78)
+@store2.employees.create(first_name: "Masha", last_name: "Bear", hourly_rate: 100)
